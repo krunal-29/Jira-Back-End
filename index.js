1ba05bd359cbff19a52f616dddb51538f2dd95c2
krunal-29 <krunalmistry7545@gmail.com>
2024-03-31 11:49:08 +0530

    cors

--- index.js
+++ index.js
@@ -11,8 +11,16 @@ app.use("/User",UserRoute)
 app.use('/Project',projectRoute)
 app.use('/Compny',CompanyRoutes)
 app.use('Employe',employeroute)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    next();
+  });
 
-
-app.listen(3000,()=>{
+app.listen(5000,()=>{
     console.log("port is run on  3000")
 })
